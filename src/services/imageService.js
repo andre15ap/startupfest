afd1b7e12293fac90fe7a5c21b37afc7b7a029ed
andre_ap <andre_ap@uft.edu.br>
2020-04-12 11:21:16 -0300

    convert http to https url image

--- src/services/imageService.js
+++ src/services/imageService.js
@@ -1,9 +1,20 @@
 import IMAGES from '../config/images';
 export const getUrl = async imageUrl => {
   try {
-    await fetch(imageUrl);
-    return { uri: imageUrl };
+    const imageHttps = getHttps(imageUrl);
+    await fetch(imageHttps);
+    return { uri: imageHttps };
   } catch (e) {
     return IMAGES.DEFAULT;
   }
+};
+
+const getHttps = imageUrl => {
+  if (imageUrl[4] !== 's') {
+    const http = imageUrl.slice(0, 4);
+    const url = imageUrl.slice(4);
+    const https = `${http}s${url}`;
+    return https;
+  }
+  return imageUrl;
 };
